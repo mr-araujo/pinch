1443587e4b14ed37951295389161734a28915d78
Murillo R. Araújo <mr_araujo@icloud.com>
2023-03-23 18:45:37 -0300

    Add double tap gesture on Content View

---
+++ Pinch/Scenes/ContentView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Murillo R. Araújo on 21/03/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var isAnimating = false
+    @State private var imageScale = CGFloat(1)
+
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2) {
+                        if imageScale == 1 {
+                            withAnimation(.spring()) {
+                                imageScale = 5
+                            }
+                        } else {
+                            withAnimation(.spring()) {
+                                imageScale = 1
+                            }
+                        }
+                    }
+            }
+            .navigationTitle("Pinch & Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            .onAppear {
+                withAnimation(.linear(duration: 1)) {
+                    isAnimating = true
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
